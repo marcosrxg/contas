a28fd982e06e26eccc60e61be7d963d6ba0d2cca
marcosrxg <marcosr_xg@hotmail.com>
2015-10-24 15:04:34 -0200

    Update lista_pac.php

--- lista_pac.php
+++ lista_pac.php
@@ -8,7 +8,7 @@
 <h1>Lista Paciente</h1>
 
 <?php
-	$pdo = new PDO('mysql:host=localhost;dbname=conta','root','contas');
+	$pdo = new PDO('mysql:host=localhost;dbname=u629263801_conta','u629263801_root','contas');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$stmt = $pdo->prepare('SELECT * FROM paciente'); 
 	$stmt->execute();
