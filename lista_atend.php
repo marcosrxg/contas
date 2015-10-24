b67c96c7e335a24d78f2e2f4daf82b64caef3b52
marcosrxg <marcosr_xg@hotmail.com>
2015-10-24 15:03:25 -0200

    Update lista_atend.php

--- lista_atend.php
+++ lista_atend.php
@@ -8,7 +8,7 @@
 <h1>Lista Atendimento</h1>
 
 <?php
-	$pdo = new PDO('mysql:host=localhost;dbname=conta','root','contas');
+	$pdo = new PDO('mysql:host=localhost;dbname=u629263801_conta','u629263801_root','contas');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$stmt = $pdo->prepare('SELECT a.nrAtendimento, 
 								  p.dsPessoaFisica, 
